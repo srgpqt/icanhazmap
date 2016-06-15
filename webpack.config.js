18e0f0fbfdcd5afd81ac26138f91fcc275c835b3
Serge Paquet <srgpqt@gmail.com>
2016-06-14 22:26:49 -0400

    Rename "lib" directory to "src"

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
 	devtool: 'source-map',
-	entry: './lib/index.js',
+	entry: './src/index.js',
 	output: {
 		path: path.join(__dirname, 'dist'),
 		filename: 'micromap.js',
